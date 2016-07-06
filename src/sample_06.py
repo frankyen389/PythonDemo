ee404c84d71b723637ada78f564564d95b063805
frak_yen <frank_yen@cht.com.tw>
2016-07-06 16:14:09 +0800

    Modify New Python

--- src/sample_06.py
+++ src/sample_06.py
@@ -11,9 +11,21 @@ def showScope():
     x = "local"
     return x
 
+def takeInt():
+    try:
+        num = int(input("請輸入整數:"))
+    except ValueError:
+        print "這不是整數"
+    else:
+        return num    
+            
+
 if __name__ == '__main__':
     print x
     print showScope()
     print x
+    num = takeInt()
+    print num
+    
 
 
